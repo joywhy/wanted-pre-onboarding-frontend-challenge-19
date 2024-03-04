d530fb97452807b3daf284a156c6af17844f3a1f
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-03-04 20:42:24 +0900

    feat:app 설계

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,12 +1,29 @@
 
+import { useState } from 'react'
 import './App.css'
-
+const Input =()=>{
+  
+  return <>
+  <input type="text"  />
+  <button type="submit" >Add</button>
+  </>
+}
+const View =()=>{
+  return <>
+  </>
+}
 function App() {
+const [inputData,setInputData] = useState([]);
+const addDate = (newInput)=>{
+  const list = inputData.push(newInput);
+  setInputData(list);
+}
 
 
   return (
     <>
-    
+    <Input addDate ={addDate}/>
+    <View list = {inputData}/>
     
     </>
   )
